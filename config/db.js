58d552139e33cae8838a48c1fe557f076180ce92
IArmandoOlmosI <armandojos3204@gmail.com>
2025-05-20 10:27:05 -0600

    Actualizando db

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-// Crear pool de conexiones
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '1234',
-  database: 'secretariaTecnica',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306,
+  ssl: { 
+    rejectUnauthorized: false,
+  },
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
 });
 
-module.exports = pool.promise();
+module.exports = pool;
